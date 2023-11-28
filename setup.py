4814dd2d7e341c903fdecdfdfd753df0bb3f19c6
Lance Kindle <lance.kindle@gmail.com>
2023-11-27 22:44:43 -0800

    consolidate variable in cronjob setup

--- setup.py
+++ setup.py
@@ -11,22 +11,24 @@ tv_off_hours=[22,]
 # time for turning tv on and off. Can put in multiple times
 
 with CronTab(user='root') as cron:
-    cron.remove_all(comment='auto-added')
+    # every time this is run, we reconstruct the CRON JOBS (but only ones that were auto-added here
+    AUTO_ADDED = 'auto-added'
+    cron.remove_all(comment=AUTO_ADDED)
     samba_script = os.path.join(SCRIPT_HOME, 'mount_samba.sh')
-    job = cron.new(command=samba_script, comment='auto-added')
+    job = cron.new(command=samba_script, comment=AUTO_ADDED)
     job.every_reboot()
 
 
     for on_hour in tv_on_hours:
-        job = cron.new(command=os.path.join(SCRIPT_HOME, 'tv_on.sh'), comment='auto-added')
+        job = cron.new(command=os.path.join(SCRIPT_HOME, 'tv_on.sh'), comment=AUTO_ADDED)
         job.hour.on(on_hour)
-        slideshow = cron.new(command=os.path.join(SCRIPT_HOME, 'run.py'), comment='auto-added')
+        slideshow = cron.new(command=os.path.join(SCRIPT_HOME, 'run.py'), comment=AUTO_ADDED)
         slideshow.hour.on(on_hour)
     
     for off_hour in tv_off_hours:
-        job = cron.new(command=os.path.join(SCRIPT_HOME, 'tv_off.sh'), comment='auto-added')
+        job = cron.new(command=os.path.join(SCRIPT_HOME, 'tv_off.sh'), comment=AUTO_ADDED)
         job.hour.on(off_hour)
-        kill_slideshow = cron.new(command=os.path.join(SCRIPT_HOME, 'stop.sh'), comment='auto-added')
+        kill_slideshow = cron.new(command=os.path.join(SCRIPT_HOME, 'stop.sh'), comment=AUTO_ADDED)
         kill_slideshow.hour.on(off_hour)
 
     
