4a6970aea78d5826618a45f0d4eb023d1ea4266f
Lance Kindle <lance.kindle@gmail.com>
2023-11-27 22:44:21 -0800

    run VLC on setup to get rid of popup that asks about metadata network access

--- setup.sh
+++ setup.sh
@@ -5,11 +5,22 @@ pip install python-vlc python-crontab
 
 sudo python setup.py  # for other setup, needs to be run with sudo to edit crontab
 
+echo ""
+echo ""
+echo ""
 echo "---------------------------------"
 echo "---------------------------------"
 echo "---------------------------------"
+echo "---------------------------------"
+echo "---------------------------------"
+echo ""
+echo ""
+echo ""
 echo "you need to set settings on panel to make it minimize to 0 pixels when not in use (as fading between pictures shows the application panel otherwise"
 echo "---------------------------------"
 echo "also need to edit /etc/lightdm/lightdm.conf and add under the [Seat:*] section"
 echo "xserver-command=X -s 0 dpms"
 echo "(it's disabling the screen blanking)"
+
+vlc  >/dev/null 2>&1 # first run pops up the question about metadata. Thereafter it won't. We need to trigger this or else it'll constantly do it during the slideshow
+
