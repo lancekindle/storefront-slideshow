a2ec94064a76dee8ae69c8fafa3ec4a0a69f3eec
Lance Kindle <lance.kindle@gmail.com>
2023-11-27 22:45:32 -0800

    rewrite of slideshow

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python
 
+'''
+    this program will display images and videos in a slideshow. It uses FEH and VLC. It also launches an image sync while it runs, to continue copying images.
+    At this point in time, it will not see those newly synced images until another run
+'''
+
 import vlc
 import time
 import subprocess
@@ -13,14 +18,26 @@ def get_screen_resolution():
     xrandr = subprocess.run("xrandr | grep -w connected | awk '{print $4}' | cut -d'+' -f1", shell=True, capture_output=True, text=True)
     return xrandr.stdout.strip()
 
-RESOLUTION = get_screen_resolution()
-FEH = ['feh', f'-ZYD {DELAY_BETWEEN_PICS*2}', '--geometry', f'{RESOLUTION}', '--auto-zoom', '--borderless', '--scale-down', '--image-bg', 'black', '--on-last-slide=quit']
-
 def enable_smooth_effects():
     # starts program that smooths transition between windows
     subprocess.run('pkill compton', shell=True)
     sub = subprocess.Popen(['compton', '-cfD', '10'])
     pass
+
+RESOLUTION = get_screen_resolution()
+# FEH can be used to show a slideshow. But here we abuse it to show one image then re-launch FEH. The reason for doing this is that each time we launch a new instance of feh,
+# it'll materialize with a smooth, subtle fade-in affect due to compton (above program). I really worked hard to make this happen, as I think it's looks much better
+# than FEH's sudden transition between images during a slideshow.
+FEH = ['feh', f'-ZYD {DELAY_BETWEEN_PICS*2}', '--geometry', f'{RESOLUTION}', '--auto-zoom', '--borderless', '--scale-down', '--image-bg', 'black', '--on-last-slide=quit']
+
+# variables used for showing images in a seamless manner
+global time_image_displayed
+global showing_image
+global obscured_image
+time_image_displayed = time.time() - DELAY_BETWEEN_PICS
+showing_image = subprocess.Popen(['sleep', '0'])
+obscured_image = subprocess.Popen(['sleep', '0'])
+
 
 def download_media(local_uri, local_folder):
     # if the trailing slashes are missing, rsync may copy local_uri source folder inside the local folder
@@ -40,97 +57,68 @@ def trigger_file_watcher(image):
     return  # it works! Don't need to do this anymore
     subprocess.Popen(['cp', image, '9' + image])
 
-def play_slideshow(image_files, shown_files):
-    shown_files.update(*image_files)
-    trigger_file_watcher(image_files[0])
-    # split images so that alternating between feh instances will show images in order
-    half1 = image_files[::2]
-    half2 = image_files[1::2]
-    # in order for FEH to work, an image has to be supplied as last argument
-    sub = subprocess.Popen(FEH + [image_files[0]])
-    time_image_displayed = time.time()
-    for image in image_files[1:]:
-        old_sub = sub
-        time_remains_before_next_pic = DELAY_BETWEEN_PICS - (time.time() - time_image_displayed)
-        if time_remains_before_next_pic > 0:
-            time.sleep(time_remains_before_next_pic)
-        sub = subprocess.Popen(FEH + [image])
-        time_image_displayed = time.time()  # reset timer
-        if image == image_files[-1]:
-            # for last file, we can exit early, but return old_sub, to make sure that finishes before we begin next set of images
-            image_files.clear()
-            return old_sub
-        old_sub.wait()  # wait for old (not showing) feh to exit. This will be after 10 seconds of running, 5 seconds of those was with the new feh displaying it's new image
-    # we should never get here, but just in case
-    image_files.clear()
-    return
+def show_picture(image):
+    global time_image_displayed
+    global showing_image
+    global obscured_image
 
-banned_filetypes = ['sh', 'py']
+    # trigger_file_watcher(image)  # enable to endlessly copy a file -- easy way to test that file-watcher works
+    
+    time_remains_before_next_pic = DELAY_BETWEEN_PICS - (time.time() - time_image_displayed)
+    if time_remains_before_next_pic > 0:
+        time.sleep(time_remains_before_next_pic)
+
+    # we launch new FEH instance over still-displaying instance because we cannot guarantee how long it'll take to load image. We want a seemless transition between images.
+    obscured_image = showing_image  # currently shown image is about to be obscured by new feh instance.
+    showing_image = subprocess.Popen(FEH + [image])
+    time_image_displayed = time.time()
+    
+
+BANNED_FILETYPES = ['sh', 'py']
+
+def find_new_files(file_list, slideshow_folder, shown_files, file_watcher):
+    if not file_list or file_watcher.stdout.read():
+        file_list = os.listdir(slideshow_folder)
+        file_list = [f for f in file_list if f not in shown_files]
+        file_list = [f for f in file_list if os.path.isfile(f) and not f.startswith('.')]  # do not want folders or hidden files
+        file_list.sort()
+        file_list.reverse()  # oldest filenames will be .pop()'d first
+    return file_list
 
 def show_all_files(slideshow_folder):
     file_watcher = subprocess.Popen(['inotifywait', '-e', 'create', '-e', 'delete', '-m', '-r', slideshow_folder], stdout=subprocess.PIPE)  # watch for new files / deleted files
     os.set_blocking(file_watcher.stdout.fileno(), False)  # on unix systems only; ensures that stdout.read() won't block if there's no output
-    file_watcher.stdout.read()  # clear any initial output
     shown_files = set()  # keep track of shown files
     os.chdir(slideshow_folder)
-    file_list = os.listdir(slideshow_folder)
-    file_list.sort()  # python sort by name  (here's where we could add additional info like created as well to prioritize newer pictures)
-    file_list.reverse()  # so that smaller numbers are shown first
+    print('at file list')
+    file_list = find_new_files([], slideshow_folder, shown_files, file_watcher)
+    
+    print('file_list', file_list)
     image_files = []
-    still_running = None  # for holding a process that can get .wait()'d on
     while file_list:
-        # loop through files, creating a list of 10 images to display at once with feh, then display them. Or display images gathered and then play video if video was found. Or display remaining images if that's all there is
-        file = file_list.pop()
-        if file in shown_files:
-            file_list = [f for f in file_list if f not in shown_files]
-        valid = True  # set to False if it's not a valid image or video file
-        video = False  # set to True to show images first, then play video
-        # we do not want folders or hidden files
-        if not os.path.isfile(file) or file.startswith('.'):
-            valid = False
+        file = file_list.pop()  # since we pop, we show larger filenames first
         name_and_ext = file.split('.')
         filetype = name_and_ext[-1]
-        if filetype in banned_filetypes:
-            valid = False
-        if valid and filetype.lower() in ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'tif', 'svg', 'webp', 'heic', 'heif', 'raw']:
-            image_files.append(file)
+        if filetype in BANNED_FILETYPES:
             shown_files.add(file)
-        elif valid:  # vlc will show a picture too, but it's not as clean
-            video = True
+        elif filetype.lower() in ['jpggglg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'tif', 'svg', 'webp', 'heic', 'heif', 'raw']:
+            show_picture(file)
             shown_files.add(file)
-
-        # now show the images
-        # showing the images also clears the list, and adds to shown_files
-        if image_files and not file_list:
-            still_running = play_slideshow(image_files, shown_files)
-        if len(image_files) >= 10:
-            still_running = play_slideshow(image_files, shown_files)
-        if video and image_files:
-            still_running = play_slideshow(image_files, shown_files)
-        if video:
+        else:  # vlc will show a picture too, but it's not as clean
             video_file = os.path.join(slideshow_folder, file)
             play_video(video_file)
-        
-        # now we've either shown all the files, or we need to keep looping
-        # while final image (still_running) shows, we refetch folder in case rsync downloaded new ones
-        if still_running:
-            are_new_files = file_watcher.stdout.read()
-            if len(image_files) == 0 and are_new_files:
-                print(are_new_files)
-                file_list = os.listdir(slideshow_folder)
-                file_list = [f for f in file_list if f not in shown_files]
-                file_list.sort()
-            still_running.wait()
-            still_running = None
+            shown_files.add(file)
+
+        file_list = find_new_files(file_list, slideshow_folder, shown_files, file_watcher)
 
 
 if __name__ == '__main__':
         enable_smooth_effects()
-        rsync_download_from = r'/tmp/smb/'
+        rsync_download_from = r'/tmp/smb/'  # on startup, this directory will be a SAMBA share
         USER = getpass.getuser()
         slideshow_folder = f'/home/{USER}/Pictures'  # where pictures get put and parsed
-    #while True:
-        download_media(rsync_download_from, slideshow_folder)
-        show_all_files(slideshow_folder)
+        for _ in range(2):
+            download_media(rsync_download_from, slideshow_folder)
+            show_all_files(slideshow_folder)
     
 
